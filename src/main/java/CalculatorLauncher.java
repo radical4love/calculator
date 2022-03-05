c5a1bec9d6a7a8341c1a7d7ccfd564bea4cbec58
radical4love <abieleninik@wp.pl>
2022-03-05 22:25:18 +0100

    komantarz

--- src/main/java/CalculatorLauncher.java
+++ src/main/java/CalculatorLauncher.java
@@ -2,4 +2,10 @@ public class CalculatorLauncher {
     public static void main(String[] args){
         System.out.println("Coming soon...");
     }
+    public static int addition(int a, int b){
+        return a-b;
+    }
+    public static int subtraction(int a, int b){
+        return a-b;
+    }
 }
