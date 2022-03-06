df2b5d09944c25ce6de7e912534c71f0cd2a7442
radical4love <abieleninik@wp.pl>
2022-03-06 18:57:09 +0100

    komantarz

--- src/main/java/CalculatorLauncher.java
+++ src/main/java/CalculatorLauncher.java
@@ -1,11 +1,24 @@
-public class CalculatorLauncher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class CalculatorLauncher extends Application {
     public static void main(String[] args){
         System.out.println("Coming soon...");
+        launch(args);
     }
-    public static int addition(int a, int b){
-        return a-b;
-    }
-    public static int subtraction(int a, int b){
-        return a-b;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+   //    FXMLLoader loader = FXMLLoader.load(getClass().getClassLoader().getResource(
+    //           "res2.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("res2.fxml"));
+        AnchorPane pane = loader.load();
+        Scene scene  = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Controller {
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+    @FXML
+    private TextField a_editable;
+
+    @FXML
+    private Button addition_choice;
+
+    @FXML
+    private TextField b_editable;
+
+    @FXML
+    private Text result;
+
+    @FXML
+    private Button subtraction_choice;
+
+    @FXML
+    void add(MouseEvent event) {
+        int a;
+        int b;
+        int r;
+        a=Integer.parseInt(a_editable.getText());
+        b=Integer.parseInt(b_editable.getText());
+       r = a+b;
+                result.setText(String.valueOf(r));
+                System.out.println(result.getText());
+    }
+
+    @FXML
+    void subtract(MouseEvent event) {
+        int a;
+        int b;
+        int r;
+        a=Integer.parseInt(a_editable.getText());
+        b=Integer.parseInt(b_editable.getText());
+        r = a-b;
+        result.setText(String.valueOf(r));
+        System.out.println(result.getText());
+    }
+
 }
